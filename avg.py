7745dda08bf25b0c7e5a85bac2e670fc75a3a5fd
Sharad <sharadtiwari@gmail.com>
2019-01-16 16:20:09 +0530

    minor changes in avg func

--- avg.py
+++ avg.py
@@ -2,17 +2,17 @@
 
 def avg(list):
     #list = [3,7,5,2,1,6]
-    print "list is: %s " %list
+    print ("list is: %s " %list)
     i = 0
     sum = 0
     while i < len(list):
         sum = sum + list[i]
-        #print "sum %d: " %sum
+        print ("sum %d: " %sum)
         i +=  1
     #avg = sum/float(len(list))
     avg = sum/len(list)
     #print "Avg of the list is %.2f " %avg
-    print "Avg of the list is %d " %avg
+    print ("Avg of the list is %d " %avg)
     return avg
 
 avg([3,7,5,2,1,6])
